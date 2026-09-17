325dd380ceec8621013461865b1608bab0cb3e22
rsak0730-cmyk <321288827+rsak0730-cmyk@users.noreply.github.com>
2026-09-17 20:31:52 +0530

    Refactor formatMs function for clarity

--- app/src/main/java/com/example/dynamicisland/util/FormatUtils.kt
+++ app/src/main/java/com/example/dynamicisland/util/FormatUtils.kt
@@ -2,9 +2,9 @@ package com.example.dynamicisland.util
 
 object FormatUtils {
     fun formatMs(ms: Long): String {
-        val sec = (ms / 1000).coerceAtLeast(0)
-        val m = sec / 60
-        val s = sec % 60
-        return "%02d:%02d".format(m, s)
+        val totalSeconds = (ms / 1000).coerceAtLeast(0)
+        val minutes = totalSeconds / 60
+        val seconds = totalSeconds % 60
+        return "%02d:%02d".format(minutes, seconds)
     }
 }
