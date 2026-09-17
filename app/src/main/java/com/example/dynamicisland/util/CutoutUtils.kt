b2b7060e1b3681b8a5b9476b3abd452608cb2f64
rsak0730-cmyk <321288827+rsak0730-cmyk@users.noreply.github.com>
2026-09-17 20:32:20 +0530

    Use Build.VERSION.SDK_INT for version check

--- app/src/main/java/com/example/dynamicisland/util/CutoutUtils.kt
+++ app/src/main/java/com/example/dynamicisland/util/CutoutUtils.kt
@@ -1,12 +1,13 @@
 package com.example.dynamicisland.util
 
 import android.app.Activity
+import android.os.Build
 import android.view.WindowInsets
 
 object CutoutUtils {
     fun topInset(activity: Activity): Int {
         val insets = activity.window.decorView.rootWindowInsets
-        return if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
             insets?.getInsets(WindowInsets.Type.displayCutout())?.top ?: 0
         } else 0
     }
